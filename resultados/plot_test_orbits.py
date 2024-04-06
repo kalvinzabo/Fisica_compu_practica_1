db9066c98e1ce5ed036001dd743f3d15e3c9ecc4
anardgz01 <anarodriguezartacho@gmail.com>
2024-04-06 18:10:00 +0200

    Add geocentric function
    Also make plot fancier

--- resultados/plot_test_orbits.py
+++ resultados/plot_test_orbits.py
@@ -6,29 +6,61 @@ import numpy as np
 
 def myplot():
     pos = np.load('resultados/pos_global.npy')
-    mainplot(pos)
+    sunplot(pos)
 
-def mainplot(pos):
+def sunplot(pos):
     # vel = np.load('vel_global')
     # a = np.load('ac_global')
-
-    pos -= pos[:, 3, np.newaxis]    #comment this line for heliocentric model
     
-    fig, axs = plt.subplots(ncols=2, figsize=(10, 10))
+    fig, axs = plt.subplots(ncols=2, figsize=(20, 20))
 
+    colors = ['gold', 'lightsteelblue', 'orange', 'olivedrab', 'indianred']
     planet_names = ['Sol', 'Mercurio', 'Venus', 'Tierra', 'Marte']
     for i in range(1, 5):
-        axs[0].plot(pos[:, i, 0], pos[:, i, 1], label=f'{planet_names[i]}')
-        
+        axs[0].plot(pos[:, i, 0], pos[:, i, 1], label=f'{planet_names[i]}', color=colors[i])
+
+    colors = ['peru', 'moccasin', 'lightcyan', 'royalblue']    
     planet_names_ext = ['Júpiter','Saturno', 'Urano', 'Neptuno']
     for i in range(5, 9):
-        axs[1].plot(pos[:, i, 0], pos[:, i, 1], label=f'{planet_names_ext[i-5]}')
+        axs[1].plot(pos[:, i, 0], pos[:, i, 1], label=f'{planet_names_ext[i-5]}', color=colors[i-5])
 
     axs[0].set_title('Órbita de los planetas interiores respecto al Sol')
     axs[1].set_title('Órbita de los planetas exteriores respecto al Sol')
 
     for ax in axs:
-        ax.scatter(0, 0, color='black', label='Sol')
+        ax.scatter(0, 0, color='gold', label='Sol')
+        ax.set_aspect('equal')
+        ax.set_xlabel('Posición X (UA)')
+        ax.set_ylabel('Posición Y (UA)')
+        ax.legend()
+
+    plt.show()
+
+def earthplot(pos):
+    # vel = np.load('vel_global')
+    # a = np.load('ac_global')
+
+    pos -= pos[:, 3, np.newaxis]
+    
+    fig, axs = plt.subplots(ncols=2, figsize=(20, 20))
+
+    colors = ['gold', 'lightsteelblue', 'orange', 'olivedrab', 'indianred']
+    planet_names = ['Sol', 'Mercurio', 'Venus', 'Tierra', 'Marte']
+    for i in range(4,-1,-1):
+        if i == 3:
+            continue
+        axs[0].plot(pos[:, i, 0], pos[:, i, 1], label=f'{planet_names[i]}', color=colors[i])
+
+    colors = ['peru', 'moccasin', 'lightcyan', 'royalblue']    
+    planet_names_ext = ['Júpiter','Saturno', 'Urano', 'Neptuno']
+    for i in range(5, 9):
+        axs[1].plot(pos[:, i, 0], pos[:, i, 1], label=f'{planet_names_ext[i-5]}', color=colors[i-5])
+
+    axs[0].set_title('Órbita de los planetas interiores y el Sol respecto a la Tierra')
+    axs[1].set_title('Órbita de los planetas exteriores respecto a la Tierra')
+
+    for ax in axs:
+        ax.scatter(0, 0, color='olivedrab', label='Tierra')
         ax.set_aspect('equal')
         ax.set_xlabel('Posición X (UA)')
         ax.set_ylabel('Posición Y (UA)')
@@ -38,4 +70,4 @@ def mainplot(pos):
 
 if __name__ == '__main__':
     pos = np.load('pos_global.npy')
-    mainplot(pos)
+    sunplot(pos)
